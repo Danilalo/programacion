a2ab8cdf95355f860301685b04cb67b88bae2b8f
Danilalo <60589721+Danilalo@users.noreply.github.com>
2020-02-04 21:29:41 -0500

    Add files via upload

---
+++ suma_imp/src/suma_imp/Suma_imp.java
@@ -0,0 +1,23 @@
+
+package suma_imp;
+
+public class Suma_imp {
+    
+public static void main(String[] args)
+{
+    long numero=0;
+    for (int i=0;i<=100000000
+           ;i++)
+    { 
+    if(i%2==0)
+    {
+}
+    else
+    {
+       numero=numero+i;
+    }
+    }
+     System.out.println("el resultado es: "+numero);
+}
+    
+}
